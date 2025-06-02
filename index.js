4bab65842dbacff1272dcb8455471f530bb57775
Gareque <grigeral@gmail.com>
2025-06-02 21:39:24 +0100

    Further changes to the code

--- index.js
+++ index.js
@@ -4,18 +4,36 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const submissions = [];
+
 app.use(express.static("public"));
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-app.post("/submit", (req, res) => {
-    res.render("index2.ejs")
+app.get("/new-chapter", (req, res) => {
+    res.render("index2.ejs");
 });
 
+app.post("/submit", (req, res) => {
+    const newChapter = {
+        chapterName: req.body.chapterName,
+        chapterColour: req.body.chapterColour,
+        chapterDescription: req.body.chapterDescription
+    };
+
+    submissions.push(newChapter);
+    console.log(newChapter);
+    res.send('Form submitted successfully');
+});
+
+app.get("/custom", (req, res) => {
+    res.json(submissions);
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+
