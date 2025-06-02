5177c9b8848e9e8319bebf3a879cab382761c8a1
Gareque <grigeral@gmail.com>
2025-06-02 20:38:02 +0100

    Adding starting files for the server

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+//import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    res.render("index2.ejs")
+});
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
