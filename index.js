fa27839dbe5d0ef6b1de00f86101e497ba63999a
Gareque <grigeral@gmail.com>
2025-06-07 11:21:28 +0100

    Adding Edit and Delete buttons

--- index.js
+++ index.js
@@ -4,7 +4,26 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-const submissions = [];
+const submissions = [
+    {
+        chapterName: "Space Wolves",
+        chapterColour: "Grey-Blue",
+        foundingChapter: "N/A",
+        chapterDescription: "Angry Space Vikings"
+    },
+    {
+        chapterName: "Blood Angels",
+        chapterColour: "Blood Red",
+        foundingChapter: "N/A",
+        chapterDescription: "Melancholic, depressed space vampires"
+    },
+    {
+        chapterName: "Dark Angels",
+        chapterColour: "Green, Beige or Black (Depending on the section)",
+        foundingChapter: "N/A",
+        chapterDescription: "Space knights, based off a mix of Arthurian legend and Biblical demons"
+    }
+];
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +47,41 @@ app.post("/submit", (req, res) => {
     submissions.push(newChapter);
     console.log(newChapter);
     res.redirect('/');
+});
+
+app.get("/edit/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const chapter = submissions[id];
+
+  if (chapter) {
+    res.render("edit-chapter.ejs", { chapter, id });
+  } else {
+    res.status(404).send("Chapter not found");
+  }
+});
+
+app.post("/edit/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (submissions[id]) {
+    submissions[id] = {
+      chapterName: req.body.chapterName,
+      chapterColour: req.body.chapterColour,
+      foundingChapter: req.body.foundingChapter,
+      chapterDescription: req.body.chapterDescription
+    };
+  }
+
+  res.redirect("/"); // or "res.redirect('/index')" if that's your route
+});
+
+// DELETE route to remove data by ID
+app.post("/delete/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!isNaN(id) && submissions[id]) {
+    submissions.splice(id, 1); // Remove the chapter at that index
+  }
+  res.redirect("/");
 });
 
 app.get("/custom", (req, res) => {
